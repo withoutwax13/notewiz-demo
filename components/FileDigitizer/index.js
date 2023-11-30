baddf93982e5587a7cbd58e778426f2c7a8077fb
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2023-12-01 01:41:12 +0800

    changed digizer order and added note regarding rate limit issues

--- components/FileDigitizer/index.js
+++ components/FileDigitizer/index.js
@@ -6,9 +6,8 @@ const FileDigitizer = ({
   setUploadedImage,
   setUploadedPDF,
   handleReset,
-  isLoading
+  isLoading,
 }) => {
-
   const handleImageUpload = (e) => {
     const file = e.target.files[0];
     setUploadedImage(file);
@@ -28,6 +27,22 @@ const FileDigitizer = ({
         <h1>1. Upload Image/PDF or Enter Text</h1>
       </div>
       <div className="option-group-container">
+        <div className="option-group">
+          <input
+            type="radio"
+            id="customInput"
+            name="uploadType"
+            value="Custom Input"
+            checked={uploadType === "Custom Input"}
+            disabled={isLoading}
+            onChange={(e) => {
+              handleReset();
+              setUploadType(e.target.value);
+            }}
+            className="radio-input data-source-utility-input"
+          />
+          <label htmlFor="customInput">Custom Input</label>
+        </div>
         <div className="option-group">
           <input
             type="radio"
@@ -60,24 +75,12 @@ const FileDigitizer = ({
           />
           <label htmlFor="pdfUpload">PDF Upload</label>
         </div>
-        <div className="option-group">
-          <input
-            type="radio"
-            id="customInput"
-            name="uploadType"
-            value="Custom Input"
-            checked={uploadType === "Custom Input"}
-            disabled={isLoading}
-            onChange={(e) => {
-              handleReset();
-              setUploadType(e.target.value);
-            }}
-            className="radio-input data-source-utility-input"
-          />
-          <label htmlFor="customInput">Custom Input</label>
-        </div>
       </div>
-
+      <div className="warning-text">
+        Note: PDF and Image upload may be unavailable in certain times due to
+        server and API limitations and may result to errors. Please use Custom
+        Input in such cases.
+      </div>
       <div className="input-container">
         {uploadType === "Image Upload" && (
           <input
